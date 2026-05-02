a28a4224a1eefa9f041c7fc7f623e18fecd64e67
sen1or <hthnam203@gmail.com>
2026-05-02 20:29:21 +0700

    feat: add @vercel/analytics dependency and integrate Analytics component into layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
@@ -66,6 +67,7 @@ export default async function RootLayout({
             </head>
             <body className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}>
                 {children}
+                <Analytics />
                 <SpeedInsights />
             </body>
         </html>
